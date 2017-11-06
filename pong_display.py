5df3c31d5f0ab9583569523eb10dcb4c4f448eb4
이준수 <ljs93kr@gmail.com>
2017-11-06 18:52:01 +0900

    pong_display: save option for saving png image

--- pong_display.py
+++ pong_display.py
@@ -1,6 +1,11 @@
 import pickle
 import os
 import matplotlib.pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--save", default=False)
+args = parser.parse_args()
 
 MODEL_HISTORY_PATH = os.path.join(".", "pong_historys")
 
@@ -9,6 +14,8 @@ historys = pickle.load(open(os.path.join(MODEL_HISTORY_PATH, 'history.p'), 'rb')
 print(type(historys))
 print(len(historys))
 print(historys)
-
 plt.plot(historys['episode_numbers'], historys['episode_rewards'], "r*", historys['episode_numbers'], historys['running_means'], 'b^')
-plt.show()
+if not args.save:
+	plt.show()
+else:
+	plt.savefig('pong_history.png')
